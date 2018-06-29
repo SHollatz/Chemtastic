4b9bcc3b3ff6517d532b8baa8f7344efd94ba9e3
SHollatz <ztallohs@gmail.com>
2018-06-28 17:39:41 -0700

    requirements done

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,8 @@
-var chemElements = ["sodium", "lithium", "hydrogen", "sulfur", "argon" ]
+var chemElements = ["Sodium", "Lithium", "Hydrogen", "Sulfur", "Aluminum", "Bismuth", "Carbon"];
 
 function renderButtons() {
-    console.log("inside renderButtons()")
-    $('#ce-view').empty();
+    //console.log("inside renderButtons()")
+    $('#buttons-view').empty();
 
     for (var i = 0; i< chemElements.length; i++) {
         var btn = $('<button>');
@@ -15,43 +15,64 @@ function renderButtons() {
 
 function displayElementInfo() {
     $('#chemElements-view').empty();
-    console.log("inside displayElementInfo()")
+    //console.log("inside displayElementInfo()")
     var element = $(this).attr('data-name');
     var apiKey = 's7Ii19O5I1mMvJO3OLiujTBrv1pMSFzB';
     var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=" + apiKey + 
     "&q=" + element+ "&limit=10&offset=0&rating=G&lang=en";
-    console.log("queryURL: " + queryURL);
+    //console.log("queryURL: " + queryURL);
 
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function(response) {
         console.dir(response);
-        var chemElementDiv = $("<div class='element'>");
         for (var i = 0; i < 10; i++) {
+            var chemElementDiv = $("<div class='element'>");
             var rating = response.data[i].rating;
-            console.log("rating: " + response.data[i].rating);
-            var p = $("<p>").text("Rating: " + rating);
+            var titleLong = response.data[i].title;
+            console.log(titleLong);
+            var shortTitle = titleLong.split('GIF',1);
+            var p = $("<p>Title: " + shortTitle + "<br>" + "Rating: "+ rating + "</p>");
             chemElementDiv.append(p);
+
             var sourceURL = response.data[i].source_post_url;
-            console.log("sourceURL: " + sourceURL);
-            var giphURL_still = response.data[i].images.original_still.url;
-            console.log("giphURL: " + giphURL_still);
+            //console.log("sourceURL: " + sourceURL);
+            var giphURL_still = response.data[i].images.downsized_still.url;
+            //console.log("giphURL_still: " + giphURL_still);
             var giph = $("<img>").attr("src", giphURL_still);
+            giph.attr("data-still", response.data[i].images.downsized_still.url);
+            giph.attr("data-animated", response.data[i].images.downsized.url);
+            giph.attr("data-state", "still");
+            giph.addClass('gif');
             chemElementDiv.append(giph);
             $('#chemElements-view').append(chemElementDiv);
         }
     })
 }
 
+function pauseAndPlay() {
+        var state = $(this).attr("data-state");
+        if (state === "still") {
+            $(this).attr("src", $(this).attr("data-animated"));
+            $(this).attr("data-state", "animate");
+        } else {
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("data-state", "still");
+        }
+}
+
 $('#addElement').on('click', function(event) {
-    console.log("inside event handler");
+    //console.log("inside event handler");
     event.preventDefault();
     var newElement = $('#ce-input').val().trim();
-    chemElements.push(newElement);
-    console.log("chemElements: " + chemElements);
-    renderButtons();
+    if (newElement.length > 0) {
+        chemElements.push(newElement);
+        //console.log("chemElements: " + chemElements);
+        renderButtons();
+    }
 })
 
 $(document).on('click', '.chemElement-btn', displayElementInfo);
+$(document).on('click', '.gif', pauseAndPlay);
 renderButtons();
