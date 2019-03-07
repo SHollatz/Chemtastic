0cd7b7856b544cbbdd34876510ff4e46fe034bb1
SHollatz <ztallohs@gmail.com>
2019-03-07 11:13:45 -0800

    edited README

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
-# giphtastic
+# Chemtastic
 App uses GIPHY API https://developers.giphy.com/docs/ to collect online giphs under the topic chemical elements. 
 
-#Purpose
+# Purpose
 This app can be used for inquiry based learning in chemistry education. The low entry level approach selects random gifs (g rated) that relate to the searched element. These include molecular models, natural appearance,experiments, production processes, areas of usage, etc. Gifs that relate only indirectly to the topic can still be fun and help to memorize names or reflect preconcepts in society. The fastest way to check for proper findings is if the element is stated in the title. If the element is not in the title,the gif maybe misleading. 
 
+# How to use it
+Click on the provided buttons on the top to check out gifs that show the element's behavior or use in the industry. 
+On the right you can insert further elements to create new buttons.
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,6 @@
 var chemElements = ["Sodium", "Lithium", "Hydrogen", "Sulfur", "Aluminum", "Bismuth", "Carbon"];
+var amount = 0;
+var searchResult = {};
 
 function renderButtons() {
     //console.log("inside renderButtons()")
@@ -13,41 +15,52 @@ function renderButtons() {
     }
 }
 
+function buildElements(amount, response) {
+    console.log("inside buildElements");
+    console.log("response: " + response);
+    for (var i = 0; i < 10 + parseInt(amount); i++) {
+        var chemElementDiv = $("<div class='element'>");
+        var rating = response.data[i].rating;
+        var titleLong = response.data[i].title;
+        console.log(titleLong);
+        var shortTitle = titleLong.split('GIF',1);
+        var p = $("<p>Title: " + shortTitle + "<br>" + "Rating: "+ rating + "</p>");
+        chemElementDiv.append(p);
+
+        var sourceURL = response.data[i].source_post_url;
+        //console.log("sourceURL: " + sourceURL);
+        var giphURL_still = response.data[i].images.downsized_still.url;
+        //console.log("giphURL_still: " + giphURL_still);
+        var giph = $("<img>").attr("src", giphURL_still);
+        giph.attr("data-still", response.data[i].images.downsized_still.url);
+        giph.attr("data-animated", response.data[i].images.downsized.url);
+        giph.attr("data-state", "still");
+        giph.addClass('gif');
+        chemElementDiv.append(giph);
+        $('#chemElements-view').append(chemElementDiv);
+    }
+}
+
 function displayElementInfo() {
     $('#chemElements-view').empty();
     //console.log("inside displayElementInfo()")
     var element = $(this).attr('data-name');
     var apiKey = 's7Ii19O5I1mMvJO3OLiujTBrv1pMSFzB';
     var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=" + apiKey + 
-    "&q=" + element+ "&limit=10&offset=0&rating=G&lang=en";
+    "&q=" + element+ "&limit=100&offset=0&rating=G&lang=en";
     //console.log("queryURL: " + queryURL);
 
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function(response) {
-        console.dir(response);
-        for (var i = 0; i < 10; i++) {
-            var chemElementDiv = $("<div class='element'>");
-            var rating = response.data[i].rating;
-            var titleLong = response.data[i].title;
-            console.log(titleLong);
-            var shortTitle = titleLong.split('GIF',1);
-            var p = $("<p>Title: " + shortTitle + "<br>" + "Rating: "+ rating + "</p>");
-            chemElementDiv.append(p);
-
-            var sourceURL = response.data[i].source_post_url;
-            //console.log("sourceURL: " + sourceURL);
-            var giphURL_still = response.data[i].images.downsized_still.url;
-            //console.log("giphURL_still: " + giphURL_still);
-            var giph = $("<img>").attr("src", giphURL_still);
-            giph.attr("data-still", response.data[i].images.downsized_still.url);
-            giph.attr("data-animated", response.data[i].images.downsized.url);
-            giph.attr("data-state", "still");
-            giph.addClass('gif');
-            chemElementDiv.append(giph);
-            $('#chemElements-view').append(chemElementDiv);
-        }
+        searchResult = response;
+        console.dir(searchResult);
+        buildElements(amount, searchResult);
+        var moreBTN = $("<button>");
+        moreBTN.attr("id", "moreBTN");
+        moreBTN.text("Show more");
+        $('#chemElements-view').append(moreBTN);
     })
 }
 
@@ -60,6 +73,10 @@ function pauseAndPlay() {
             $(this).attr("src", $(this).attr("data-still"));
             $(this).attr("data-state", "still");
         }
+}
+
+function displayMoreGifs() {
+    amount = amount + 10;
 }
 
 $('#addElement').on('click', function(event) {
@@ -75,4 +92,5 @@ $('#addElement').on('click', function(event) {
 
 $(document).on('click', '.chemElement-btn', displayElementInfo);
 $(document).on('click', '.gif', pauseAndPlay);
+$(document).on('click', '#moreBTN', );
 renderButtons();
